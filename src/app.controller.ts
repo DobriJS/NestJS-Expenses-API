e1693d4f3b7db649858a43a56b01cff35b388997
Dobri Dobrevski <53184157+DobriJS@users.noreply.github.com>
2022-09-28 12:20:14 +0300

    Initial CRUD implementation

---
+++ src/app.controller.ts
@@ -0,0 +1,77 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
+import { data, ReportType } from 'src/data';
+import { v4 as uuid } from 'uuid';
+
+@Controller('/report/:type')
+export class AppController {
+  getReportType = (type: string) =>
+    type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+  @Get()
+  getAllReports(@Param('type') type: string) {
+    const reportType = this.getReportType(type);
+    return data.report.filter((report) => report.type === reportType);
+  }
+
+  @Get(':id')
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType = this.getReportType(type);
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+  }
+
+  @Post()
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  @Put(':id')
+  updateReport(
+    @Body() body: { amount: number; souce: string },
+    @Param('id') id: string,
+    @Param('type') type: string,
+  ) {
+    const reportType = this.getReportType(type);
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
+    return;
+  }
+}
---
+++ src/data.ts
@@ -0,0 +1,44 @@
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'Food',
+      amount: 5000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+    {
+      id: 'uuid3',
+      source: 'Efbet',
+      amount: 1000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+  ],
+};
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
