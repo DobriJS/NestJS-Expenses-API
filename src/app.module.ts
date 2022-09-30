9a669fe094a584e6457ebe67149a11549ce53840
Dobri Dobrevski <53184157+DobriJS@users.noreply.github.com>
2022-10-01 00:58:32 +0300

    Generate service and summary controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SummaryModule } from './summary/summary.module';
+import { ServiceController } from './service/service.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [SummaryModule],
+  controllers: [AppController, ServiceController],
   providers: [AppService, {
     provide: APP_INTERCEPTOR,
     useClass: ClassSerializerInterceptor,
